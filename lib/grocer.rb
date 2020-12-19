1bf88ff6b878ff9f8d1d75d8de0e07be5acef68f
Alexandra R <alexandra.ricardel@gmail.com>
2020-12-19 01:38:14 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,7 +5,6 @@ def find_item_by_name_in_collection(name, collection)
    return grocery_item if grocery_item[:item] == name
    count += 1 
  end 
- find_item_by_name_in_collection("Cholula", collection)
 end
 
 def consolidate_cart(cart)
