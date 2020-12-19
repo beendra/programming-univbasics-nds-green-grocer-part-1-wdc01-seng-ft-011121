d558993b2248d2495c21616ef752eed7c68663a9
Alexandra R <alexandra.ricardel@gmail.com>
2020-12-19 01:50:56 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,18 +9,24 @@ end
 
 
 def consolidate_cart(cart)
-index = 0 
-consolidated_cart = []
-while index < cart.length do 
-  item = find_item_by_name_in_collection(cart[index][:item], consolidated_cart)
-  if item
-    consolidated_cart_index = 0 
-  while consolidated_cart_index < consolidated_cart.length do 
-    consolidated_cart[index][:count] += 1 
-  end 
-
-
-end
-
-
+  index = 0
+  consolidated_cart = []
   
+  while index < cart.length do
+    item = find_item_by_name_in_collection(cart[index][:item], consolidated_cart)
+    if item
+      consolidated_cart_index = 0
+      while consolidated_cart_index < consolidated_cart.length do
+        if consolidated_cart[consolidated_cart_index][:item] === item[:item]
+          consolidated_cart[consolidated_cart_index][:count] += 1
+        end
+        consolidated_cart_index += 1
+      end
+    else
+      cart[index][:count] = 1
+      consolidated_cart << cart[index]
+    end
+    index += 1
+  end
+  consolidated_cart
+end
