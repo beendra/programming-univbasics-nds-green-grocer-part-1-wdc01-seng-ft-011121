3f3679e943897e7fa987a177bfa5709146b0e4c0
Alexandra R <alexandra.ricardel@gmail.com>
2020-12-19 01:41:24 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,7 @@ def find_item_by_name_in_collection(name, collection)
  index = 0 
  
 while index < collection.length do
-  return collection[index]
+  return collection[index] if collection[index]
 
 
 def consolidate_cart(cart)
