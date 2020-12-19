13f825d52916b59bad72ceebabb2b1cc7cb9d859
Alexandra R <alexandra.ricardel@gmail.com>
2020-12-19 01:39:46 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,12 +1,4 @@
 def find_item_by_name_in_collection(name, collection)
- count = 0 
- 
- collection.each do |grocery_item|
-   return grocery_item if grocery_item[:item] == name
-   count += 1 
- end 
-end
-
   index = 0
 
   while index < collection.length do
