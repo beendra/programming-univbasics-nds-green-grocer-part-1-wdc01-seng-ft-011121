01e3568cb48e2707cadbc49515a8afdc4114541f
Alexandra R <alexandra.ricardel@gmail.com>
2020-12-19 01:40:26 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,11 +1,12 @@
 def find_item_by_name_in_collection(name, collection)
-  index = 0
-
-  while index < collection.length do
-    return collection[index] if collection[index][:item] === name 
-    index += 1
-  end
+ index = 0 
+ 
+ collection.each do |grocery_item|
+   return grocery_item if grocery_item[:item] == name
+   index += 1 
+ end 
 end
+
 
 def consolidate_cart(cart)
 
